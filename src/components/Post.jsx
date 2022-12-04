a541f6b494a37050823a609169eb92aa0ba40c30
Karolayne Ataide <karolayne.o.v.a@gmail.com>
2022-12-04 17:46:00 -0300

    add changes

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -35,6 +35,12 @@ export function Post() {
           <a href="#">#rocketseat</a>
         </p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea placeholder="Deixe um comentário" />
+        <button type="submit" >Comentar</button>
+      </form>
     </article>
   );
 }
